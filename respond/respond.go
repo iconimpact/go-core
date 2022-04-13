f61ab055fd8ee0a49ff5ff47efef76581633f744
Dorin Strimbu <us3r64@gmail.com>
2022-04-13 17:27:09 +0300

    Update respond func's params to include a logger Remove default struct for message response body

--- respond/respond.go
+++ respond/respond.go
@@ -1,55 +1,36 @@
 package respond
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"reflect"
 
 	"github.com/iconmobile-dev/go-core/errors"
 	jsoniter "github.com/json-iterator/go"
+	"go.uber.org/zap"
 )
 
 const jsonContentType = "application/json; charset=utf-8"
 
 var json = jsoniter.ConfigFastest
 
-// JSONMsg default struct for message response body.
-type JSONMsg struct {
-	Data interface{} `json:"data" swaggertype:"object"`
-	Msg  string      `json:"msg"`
-}
-
 // JSON serializes the given struct as JSON into the response body.
 // It also sets the Content-Type as "application/json" and
 // X-Content-Type-Options as "nosniff".
-// If v is a string then it is sent as "Msg" property value.
-func JSON(w http.ResponseWriter, r *http.Request, status int, v interface{}) {
-	var body []byte
-	var data JSONMsg
-
-	iv := reflect.ValueOf(v)
-	if iv.Kind() == reflect.Ptr {
-		iv = iv.Elem()
-	}
-
-	switch iv.Kind() {
-	case reflect.String:
-		data.Msg = v.(string)
-	default:
-		data.Data = v
-	}
-
-	body, err := json.Marshal(&data)
+// Logs the status and v if l is not nil.
+func JSON(w http.ResponseWriter, l *zap.Logger, status int, v interface{}) {
+	jsonBytes, err := json.Marshal(v)
 	if err != nil {
 		panic("respond: " + err.Error())
+	}
+
+	if l != nil {
+		l.Info("respond: ", zap.Int("status", status), zap.String("body", string(jsonBytes)))
 	}
 
 	w.Header().Set("Content-Type", jsonContentType)
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(status)
 
-	_, err = io.Copy(w, bytes.NewReader(body))
+	_, err = w.Write(jsonBytes)
 	if err != nil {
 		panic("respond: " + err.Error())
 	}
@@ -57,13 +38,14 @@ func JSON(w http.ResponseWriter, r *http.Request, status int, v interface{}) {
 
 // JSONError returns an HTTP response as JSON message with status code
 // base on app err Kind, Msg from app err HTTPMessage.
-// Logs the error by default.
-func JSONError(w http.ResponseWriter, r *http.Request, err error) {
+// Logs the error if l is not nil.
+func JSONError(w http.ResponseWriter, l *zap.Logger, err error) {
 	var status int
 	var errMsg string
 
-	// log the error by default
-	logger.Error("respond: ", err)
+	if l != nil {
+		l.Error("respond: ", zap.Error(err))
+	}
 
 	// set custom app err Message
 	appErr, ok := err.(*errors.Error)
@@ -75,5 +57,5 @@ func JSONError(w http.ResponseWriter, r *http.Request, err error) {
 		errMsg = errors.ToHTTPResponse(appErr)
 	}
 
-	JSON(w, r, status, errMsg)
+	JSON(w, nil, status, errMsg)
 }
